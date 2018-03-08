0fb36d689a0c17e862e6e813b2c93cd43be31bc0
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-03-07 22:11:15 -0600

    Refractor file structure

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import { Navigation } from 'react-native-navigation';
 import { AsyncStorage } from 'react-native';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import registerScreens from './screens';
+import registerScreens from './src/screens';
+// Due to security reason, I cannot include this file in version control
+// Please follow this link if you want to generate your own personal token:
+// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
 import { GITHUB_TOKEN } from './config';
 
 const client = new ApolloClient({
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react-native';
 // eslint-disable-next-line
-import App from 'Profile/App';
+import App from 'Profile/src/App';
 
 storiesOf('App', module).add('main', () => <App />);
