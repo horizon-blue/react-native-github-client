bd4f24527408dbb62c4cfda748d5f4fed34e36a3
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-03-10 17:58:33 -0600

    Remove storybook

--- storybook/stories/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react-native';
-// eslint-disable-next-line
-import App from 'Profile/src/Home';
-
-storiesOf('Home', module).add('main', () => <Home />);
