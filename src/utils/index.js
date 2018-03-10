2813bccdd560115ccb2e13a40e045fbfd110bdae
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-03-10 05:46:44 -0600

    Refractor warpQueries into individual file

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
 import { Linking } from 'react-native';
-import { graphql } from 'react-apollo';
 import _ from 'lodash/fp';
 
 /**
@@ -37,73 +36,4 @@ export const deepMerge = (objA, objB) =>
     return bVal;
   })(objA)(objB);
 
-/**
- * A helper function that generate a function to transform fetched result
- * to props
- * @param  {String} propName    the prop name to transformed
- * @param  {String} ownPropName the prop to receive data
- * @param  {Boolean} isViewer    whether the current query is for viewer
- * @return {function}           a function that transformed
- */
-export const transformProps = (ownPropName, propName, isViewer) => ({
-  ownProps,
-  data: { viewer, user, fetchMore, ...rest },
-}) => {
-  if (rest.loading || rest.error)
-    return {
-      data: rest,
-      fetchMore: () => {},
-    };
-  const actor = isViewer ? viewer : user;
-
-  if (!ownPropName)
-    return {
-      data: rest,
-      viewer: actor,
-    };
-
-  const props = actor[ownProps[ownPropName]];
-
-  return {
-    [propName]: _.reverse(props.edges),
-    fetchMore: () =>
-      fetchMore({
-        variables: {
-          before: _.first(props.edges).cursor,
-          login: ownProps.login,
-        },
-        updateQuery: (previousResult, { fetchMoreResult }) =>
-          deepMerge(fetchMoreResult, previousResult),
-      }),
-    data: rest,
-  };
-};
-
-/**
- * A helper function that return a warpper function that can be applied to
- * a component so that it select the query based on props[ownPropName] and
- * match the selected result to props[propName]. The returned function, when
- * evaluate on a component, should return a function that takes a list of
- * arguments to be passed in to queryGetter for each query
- * @param  {String} ownPropName the prop name to transformed
- * @param  {String} propName    the prop to receive data
- * @param  {function} queryGetter the function that should return a graphql
- *                              query when evaluated
- * @return {function}           a function in arity of two
- */
-export const warpQueries = (ownPropName, propName, queryGetter) =>
-  _.reduce((result, args) => {
-    const [field, rest] = _.isArray(args) ? args : [args, null];
-    return graphql(queryGetter(field, rest, false), {
-      skip: props =>
-        (ownPropName && props[ownPropName] !== field) || !props.login,
-      options: ({ login }) => ({ variables: { login } }),
-      props: transformProps(ownPropName, propName, false),
-    })(
-      graphql(queryGetter(field, rest, true), {
-        skip: props =>
-          (ownPropName && props[ownPropName] !== field) || props.login,
-        props: transformProps(ownPropName, propName, true),
-      })(result)
-    );
-  });
+export { default as warpQueries } from './warpQueries';
