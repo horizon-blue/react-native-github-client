55a9bd67edc1664d327cca13ea79737dcbaed9c1
Xiaoyan Wang <horizonblueblueblue@gmail.com>
2018-03-26 01:36:55 -0500

    Test Respository List

---
+++ __tests__/RepositoryList.js
@@ -0,0 +1,62 @@
+import 'react-native';
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import RepositoryListView from '../src/components/Repository/RepositoryListView';
+
+import renderer from 'react-test-renderer';
+
+const repoList = [
+  {
+    node: {
+      id: 'as123oisdf',
+      nameWithOwner: 'foo/bar',
+      name: 'bar',
+      description: '..',
+      viewerHasStarred: true,
+      primaryLanguage: null,
+      url: '',
+      isPrivate: false,
+      forkCount: 23,
+      stargazers: {
+        totalCount: 5,
+      },
+    },
+  },
+  {
+    node: {
+      id: 'asdlkf23sdf',
+      nameWithOwner: 'segmentation/fault',
+      name: 'fault',
+      description: 'something to test',
+      viewerHasStarred: false,
+      primaryLanguage: {
+        id: 'dfd2asdf',
+        color: '#0000ff',
+        name: 'whocares',
+      },
+      url: '',
+      isPrivate: true,
+      forkCount: 0,
+      stargazers: {
+        totalCount: 5,
+      },
+    },
+  },
+];
+
+it('repository list view render correctly', () => {
+  expect(
+    renderer
+      .create(
+        <ApolloProvider client={{}}>
+          <RepositoryListView
+            refetch={() => null}
+            fetchMore={() => null}
+            repositories={repoList}
+            data={{ loading: false }}
+          />
+        </ApolloProvider>
+      )
+      .toJSON()
+  ).toMatchSnapshot();
+});
